772d6620f3c09c2d636bfadf9320962e989befe0
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-16 16:51:36 +0200

    portfolio broullion

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const toggle = document.querySelector('.toggle');
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
 
-toggle.addEventListener('click', (e) => {
-    toggle.classList.remove("aside")
-    
-})
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
