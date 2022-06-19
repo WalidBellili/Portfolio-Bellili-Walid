970d354b457714d330bf9c539cfd70e039e7d4c6
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-20 00:43:07 +0200

    add caroussel

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const button = document.getElementById("btn");
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-
-button.addEventListener("click", (e) => {
-    console.log(e);
-})
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  })
